4be243d17f8945722a6d30829a03ec3e04724ed9
wayne.han <wayne.han@picovr.com>
2021-02-23 11:07:45 +0800

    optimized code

--- Assets/Project/Main.cs
+++ Assets/Project/Main.cs
@@ -5,16 +5,14 @@ using UnityEngine.UI;
 using System;
 
 
-public class Main : MonoBehaviour {
+public class Main : MonoBehaviour
+{
 
     public MeshRenderer meshRenderer;
+    public RawImage rawImage;
     private AndroidJavaObject nativeObject;
     private int width, height;
     private Texture2D texture2D;
-
-    void Awake()
-    {
-    }
 
     // Use this for initialization
     void Start()
@@ -28,11 +26,9 @@ public class Main : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        bool updateFrame = nativeObject.Call<bool>("isUpdateFrame");
-        if (updateFrame)
+        if (texture2D != null && nativeObject.Call<bool>("isUpdateFrame"))
         {
             Debug.Log("VideoPlugin:Update");
-            meshRenderer.material.mainTexture = texture2D;
             nativeObject.Call("updateTexture");
             GL.InvalidateState();
         }
@@ -41,10 +37,16 @@ public class Main : MonoBehaviour {
     public void Click()
     {
         Debug.Log("VideoPlugin:Start");
-        texture2D = new Texture2D(width, height, TextureFormat.RGB565, false, false);
-        texture2D.wrapMode = TextureWrapMode.Clamp;
-        texture2D.filterMode = FilterMode.Bilinear;
-        nativeObject.Call("start", (int)texture2D.GetNativeTexturePtr(), width, height);
+        if (texture2D == null)
+        {
+            texture2D = new Texture2D(width, height, TextureFormat.RGB24, false, false);
+            //texture2D.wrapMode = TextureWrapMode.Clamp;
+            //texture2D.filterMode = FilterMode.Bilinear;
+            //texture2D.Apply();
+            nativeObject.Call("start", (int)texture2D.GetNativeTexturePtr(), width, height);
+            meshRenderer.material.mainTexture = texture2D;
+            rawImage.texture = texture2D;
+        }
     }
 
 }
